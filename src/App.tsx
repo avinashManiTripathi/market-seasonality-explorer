761c774727ae5f365a31303b9a5a66c576bb0dd7
Avinash Mani Tripathi <avinashmanitripathi@Avinashs-MacBook-Air.local>
2025-07-22 22:36:28 +0530

    added calendar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+
+import React, { useEffect } from 'react';
 import { Box, Flex, Heading } from '@chakra-ui/react';
 import DarkModeToggle from './components/DarkModeToggle';
+import { useSetRecoilState } from 'recoil';
+import { volatilityDataAtom } from './store/calendarState';
+import CalendarView from './components/Calendar/CalendarView';
+import { format, eachDayOfInterval, subMonths, endOfMonth } from 'date-fns';
 
-function App() {
+const App = () => {
+  const setVolatility = useSetRecoilState(volatilityDataAtom);
+
+  useEffect(() => {
+    const start = subMonths(new Date(), 1);
+    const end = endOfMonth(new Date());
+
+    const days = eachDayOfInterval({ start, end });
+
+    const mockVol = days.reduce((acc, date) => {
+      const key = format(date, 'yyyy-MM-dd');
+      acc[key] = Math.random(); // random volatility
+      return acc;
+    }, {} as Record<string, number>);
+
+    setVolatility(mockVol);
+  }, []);
+
   return (
     <Box p={4}>
       <Flex justify="space-between" align="center">
         <Heading size="md">Market Seasonality Explorer</Heading>
         <DarkModeToggle />
       </Flex>
-      {/* Your app content here */}
+      <CalendarView />
     </Box>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App';
 import theme from './theme';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
